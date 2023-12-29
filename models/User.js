daee4a780e0410dfb6daee624d0cb68599dbe8b1
fantasyLink0222 <fantasylink@gmail.com>
2023-12-29 15:27:31 -0800

    draft frame done

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 // User Schema
 const userSchema = mongoose.Schema({
+  
   username: {
     type: String,
     unique: true,
